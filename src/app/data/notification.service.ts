60067b4275879e84a7cd73e9a2efaf130137f9ae
Dumisani Ndhlovu <dumisa4ndhlovu@gmail.com>
2020-02-21 13:22:33 +0200

    Bayekeleni bakhulume

--- src/app/data/notification.service.ts
+++ src/app/data/notification.service.ts
@@ -7,8 +7,8 @@ import { HttpClient } from '@angular/common/http';
 export class NotificationService {
 
   // private baseURL = 'https://social-auth-api-v1.herokuapp.com/auth';
-  private baseURL = 'https://better-life-backend.herokuapp.com/api';
-  // private baseURL = 'http://localhost:8085/api';
+  // private baseURL = 'https://better-life-backend.herokuapp.com/api';
+  private baseURL = 'http://localhost:8085/api';
 
 
   constructor(@Inject(forwardRef(() => HttpClient)) public httpClient : HttpClient) {
